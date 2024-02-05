d2070bfe9d849dcffad8e27cf9364392ef80722e
Dmitrii Kushchenko <dikushch@gmail.com>
2024-02-05 19:38:32 +0300

    feat: add isameength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -146,8 +146,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const l = arr[0].length;
+  return arr.every((e) => e.length === l);
 }
 
 /**
