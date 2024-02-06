f3afa14d915f8d45d133cd27c0206d6197ca7b99
Dmitrii Kushchenko <dikushch@gmail.com>
2024-02-06 11:17:25 +0300

    feat: add createimensionalrray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -272,8 +272,10 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  const arr = new Array(size);
+  if (n === 1) return arr.fill(0);
+  return arr.fill(createNDimensionalArray(n - 1, size));
 }
 
 /**
